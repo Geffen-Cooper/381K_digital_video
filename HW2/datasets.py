c8bf2daa90abdad55c4f94d40d6c5361b6f7afbc
Geffen Cooper <geffencooper@gmail.com>
2023-03-05 03:29:15 -0600

    support frame format

--- HW2/datasets.py
+++ HW2/datasets.py
@@ -21,71 +21,83 @@ num_frames (int): number of frames to get from the video
 
 by default the function will return the full first frame of the video
 """
-def get_video_frames(video_path,patch_size=None,patch_coord=None,num_frames=None):
-    # create video capture object
-    cap = cv2.VideoCapture(video_path)
-        
-    # Check if video opened successfully
-    if (cap.isOpened()== False): 
-        print("Error opening video file")
-        exit()
+def get_video_frames(video_path,is_img_frames,patch_size=None,patch_coord=None,num_frames=None):
+    if is_img_frames == False:
+        # create video capture object
+        cap = cv2.VideoCapture(video_path)
+            
+        # Check if video opened successfully
+        if (cap.isOpened()== False): 
+            print("Error opening video file")
+            exit()
 
-    # read the first frame
-    ret, frame = cap.read()
+        # read the first frame
+        ret, frame = cap.read()
 
-    if ret:
-        # by defaut return the first frame
-        if patch_size == None and patch_coord == None and num_frames == None:
-            # release the video capture object
-            cap.release()
-            return frame
+        if ret:
+            # by defaut return the first frame
+            if patch_size == None and patch_coord == None and num_frames == None:
+                # release the video capture object
+                cap.release()
+                return frame
 
+            else:
+                if num_frames == None:
+                    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+                # create frame buffer
+                frames = np.empty((num_frames, patch_size[1], patch_size[0], 3), np.dtype('uint8'))
+                frame_count = 0
+
+                # store the first frame
+                start_x,start_y = patch_coord[0], patch_coord[1]
+                end_x, end_y = start_x + patch_size[0], start_y + patch_size[1]
+
+                frames[frame_count] = frame[start_y:end_y,start_x:end_x,:]
+                frame_count += 1
+
+                # store the remaning frames
+                while (frame_count < num_frames and ret):
+                    start_x,start_y = patch_coord[0], patch_coord[1]
+                    end_x, end_y = start_x + patch_size[0], start_y + patch_size[1]
+                    try:
+                        frames[frame_count] = frame[start_y:end_y,start_x:end_x,:]
+                    except:
+                        print(frame_count)
+                        print(frame,ret)
+                    frame_count += 1
+                    ret, frame = cap.read()
+                return frames
         else:
-            if num_frames == None:
-                num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            print("can't get first frame")
+            exit()
+    else:
+        files = video_path
+        num_frames = len(files)
 
-            # create frame buffer
-            frames = np.empty((num_frames, patch_size[1], patch_size[0], 3), np.dtype('uint8'))
-            frame_count = 0
+        # create frame buffer
+        frames = np.empty((num_frames, patch_size[1], patch_size[0], 3), np.dtype('uint8'))
+        frame_count = 0
 
+        for file in files:
+            frame = cv2.imread(file)
             # store the first frame
             start_x,start_y = patch_coord[0], patch_coord[1]
             end_x, end_y = start_x + patch_size[0], start_y + patch_size[1]
 
             frames[frame_count] = frame[start_y:end_y,start_x:end_x,:]
             frame_count += 1
-
-            # store the remaning frames
-            while (frame_count < num_frames and ret):
-                start_x,start_y = patch_coord[0], patch_coord[1]
-                end_x, end_y = start_x + patch_size[0], start_y + patch_size[1]
-                try:
-                    frames[frame_count] = frame[start_y:end_y,start_x:end_x,:]
-                except:
-                    print(frame_count)
-                    print(frame,ret)
-                frame_count += 1
-                ret, frame = cap.read()
-            return frames
-    else:
-        print("can't get first frame")
-        exit()
-    
-# crop = get_video_frames("chaplin.mp4",(224,224),(100,100),100)
-# print(crop.shape)
-# for f in crop:
-#     cv2.imshow('test',f)
-#     cv2.waitKey()
-# cv2.waitKey()
+        return frames
 
 class VideoPairs(Dataset):
     """Video Pairs Dataset.
     """
 
-    def __init__(self, root_dir,patch_size=224,overlap_ratio=0.5,transform=None,training=True,validation=False,testing=False):
+    def __init__(self, root_dir,img_frames=True,patch_size=224,overlap_ratio=0.5,transform=None,training=True,validation=False,testing=False):
         """
         Args:
             root_dir (string): directory where the training/validation/test splits are
+            img_frames (bool): videos are stored as individual frames rather than a video file
             patch_size (int): the crop dimension to use
             overlap_ratio (float): the amount of overlap between crops
             transform (callable, optional): transform to be applied on a sample
@@ -94,37 +106,55 @@ class VideoPairs(Dataset):
             testing (bool): specify if to load the testing data
         """
         self.root_dir = root_dir
+        self.img_frames = img_frames
         self.transform = transform
         self.patch_size = patch_size
         self.overlap_ratio = overlap_ratio
 
         if training:
-            self.sub_dir = "training set"
+            self.sub_dir = "training"
         elif validation:
-            self.sub_dir = "validation set"
+            self.sub_dir = "validation"
         elif testing:
-            self.sub_dir = "testing set"
+            self.sub_dir = "testing"
 
         self.video_path = os.path.join(self.root_dir,self.sub_dir)
-        self.comp_video_files = []
-        self.gt_video_files = []
-        for f in os.listdir(os.path.join(self.video_path,"compressed")):
-            self.comp_video_files.append(os.path.join(self.video_path,"compressed",f))
-        for f in os.listdir(os.path.join(self.video_path,"ground truth")):
-            self.gt_video_files.append(os.path.join(self.video_path,"ground truth",f))
 
-        self.num_videos = len(self.comp_video_files)
-        print(self.comp_video_files)
-        print(self.gt_video_files)
+        if img_frames == False:
+            self.comp_video_files = []
+            self.gt_video_files = []
+            for f in os.listdir(os.path.join(self.video_path,"compressed")):
+                self.comp_video_files.append(os.path.join(self.video_path,"compressed",f))
+            for f in os.listdir(os.path.join(self.video_path,"GT")):
+                self.gt_video_files.append(os.path.join(self.video_path,"GT",f))
+
+            self.num_videos = len(self.comp_video_files)
+            first_frame = get_video_frames(self.comp_video_files[0],False) 
+        else:
+            # store files for each subfolder
+            subfolders = os.listdir(os.path.join(self.video_path,"compressed"))
+            subfolders = sorted(subfolders,key=lambda x: int(os.path.splitext(x)[0]))
+
+            self.comp_video_files = [[] for i in range(len(subfolders))]
+            self.gt_video_files = [[] for i in range(len(subfolders))]
+
+            for i,folder in enumerate(subfolders):
+                files = os.listdir(os.path.join(self.video_path,"compressed",folder))
+                files = sorted(files,key=lambda x: int(os.path.splitext(x)[0]))
+                
+                for file in files:
+                    self.comp_video_files[i].append(os.path.join(self.video_path,"compressed",folder,file))
+                    self.gt_video_files[i].append(os.path.join(self.video_path,"GT",folder,file))
+
+            self.num_videos = len(self.comp_video_files)
+            first_frame = cv2.imread(self.comp_video_files[0][0])
 
         # We need to generate a mapping from the sample idx to a video crop
         # For each video we will identify how many 224x224 regions fit in the video WxH,
         # using an overlap of 50%. If the WxH is not divisible by 128, then for the last
         # crop just fit the remaining portion.
-
-        # first we need to get the video dimensions by reading one video frame
-        first_frame = get_video_frames(self.comp_video_files[0])
-        print(first_frame.shape)
+        
+        # print(first_frame.shape)
         self.vid_h, self.vid_w = first_frame.shape[:2]
 
         # now get the coordinates of each patch using the given overlap ratio
@@ -135,7 +165,6 @@ class VideoPairs(Dataset):
         # patch doesn't fit evenly
         if (self.vid_w - patch_size) % overlap_px != 0:
             num_patches_horizontal += 1
-            # print(self.vid_w,np.arange(0,self.vid_w-patch_size,overlap_px),np.array([self.vid_w-patch_size]))
             x_coords = np.concatenate([np.arange(0,self.vid_w-patch_size,overlap_px),np.array([self.vid_w-patch_size])])
         else:
             x_coords = np.arange(0,self.vid_w,overlap_px)
@@ -163,8 +192,8 @@ class VideoPairs(Dataset):
         y_coord = self.patch_coords_y[y_coord,0]
 
         # read video patch, need to convert to tensor
-        compressed = get_video_frames(self.comp_video_files[video_number],(self.patch_size,self.patch_size),(x_coord,y_coord))
-        ground_truth = get_video_frames(self.gt_video_files[video_number],(self.patch_size,self.patch_size),(x_coord,y_coord)) 
+        compressed = get_video_frames(self.comp_video_files[video_number],self.img_frames,(self.patch_size,self.patch_size),(x_coord,y_coord))
+        ground_truth = get_video_frames(self.gt_video_files[video_number],self.img_frames,(self.patch_size,self.patch_size),(x_coord,y_coord)) 
         
         return compressed, ground_truth
 
@@ -172,7 +201,8 @@ class VideoPairs(Dataset):
         return self.patch_coords_x.shape[0]*self.patch_coords_x.shape[1]*self.num_videos
 
     def visualize_sample(self):
-        comp,gt = self.__getitem__(7)
+        comp,gt = self.__getitem__(30)
+        print(comp.shape,gt.shape)
         out_comp = cv2.VideoWriter('out_comp.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 30, (224,224))
         out_gt = cv2.VideoWriter('out_gt.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 30, (224,224))
 
@@ -196,7 +226,7 @@ class VideoPairs(Dataset):
                 cv2.imshow('comp',frame_comp)
                 cv2.imshow('gt',frame_gt)
                 
-                if cv2.waitKey(10) & 0xFF == ord('q'):
+                if cv2.waitKey(40) & 0xFF == ord('q'):
                     break
             
             # Break the loop
@@ -205,6 +235,6 @@ class VideoPairs(Dataset):
                 cap_gt.set(cv2.CAP_PROP_POS_FRAMES, 0)
                 # break 
 
-vd = VideoPairs("C:\\Users\\geffen\\Documents\\Programming\\381K_digital_video\\HW2",224,0.5,None,training=True)
+vd = VideoPairs("C:\\Users\\geffen\\Documents\\Programming\\381K_digital_video\\HW2",True,224,0.5,None,training=False,testing=True)
 print(len(vd))
 vd.visualize_sample()
---
+++ HW2/models.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+
+# model for reducing compression artifacts in videos
+class ArtifactReduction(nn.Module):
+    def __init__(self):
+        super(ArtifactReduction, self).__init__()
+
+        # input volume is (N x C X D x H x W)
+        # N (batch size) = 1
+        # C (channel) = 3 RGB
+        # D (depth) = input dependent, number of frames
+        # H,W (height, width) = input dependent
+
+        # we want to keep the dimensionality to avoid loosing spatial and temporal
+        # information since the output is also a video volume
+
+        # the kernel is 3D now where the 3rd dimension corresponds to how many frames we
+        # want to convolve over since the kernel also slides in the time dimension
+
+        # in the same way that the H,W are implicit in 2D, the H,W,D are implicit in 3D
+        # so the filter will slide in all three dimensions depending on stride and padding,
+        # use stride 1 and padding 1 to keep the volume the same dimensions
+
+        # the number of output channels corresponds to the number of filters since each filter
+        # will collapse a volume into a single channel as it did in 2D 
+
+        # (1 x 3 x 100 x 224 x 224) --> (1 x 16 x 100 x 224 x 224)
+        self.conv1 = nn.Conv3d(in_channels=3, out_channels=16, kernel_size=3, padding=1) 
+        
+        # (1 x 16 x 100 x 224 x 224) --> (1 x 32 x 100 x 224 x 224)
+        self.conv2 = nn.Conv3d(in_channels=16, out_channels=32, kernel_size=3, padding=1) 
+
+        # (1 x 32 x 100 x 224 x 224) --> (1 x 3 x 100 x 224 x 224)
+        self.conv3 = nn.Conv3d(in_channels=3, out_channels=3, kernel_size=3, padding=1) 
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+
+        x = self.conv3(x)
+
+        return x
