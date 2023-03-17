reusing computation bug

project/diamond.py
@@ -51,15 +51,34 @@ y_off = 0
 
 all_search_locs = np.array([[0,0],[0,-2],[-1,-1],[-2,0],[-1,1],[0,2],[1,1],[2,0],[1,-1]])
 c_search_locs = np.array([[0,-1],[-1,0],[0,1],[1,0]])
-l_search_locs = np.array([[2,0],[1,-1],[0,-2],[-1,-1],[-2,0]]) #np.array([7,8,1,2,3])
-tl_search_locs = np.array([[0,-2],[-1,-1],[-2,0]]) #np.array([1,2,3])
-t_search_locs = np.array([[0,-2],[-1,-1],[-2,0],[-1,1],[0,2]]) # np.array([1,2,3,4,5])
-tr_search_locs = np.array([[-2,0],[-1,1],[0,2]]) #np.array([3,4,5])
-r_search_locs = np.array([[-2,0],[-1,1],[0,2],[1,1],[2,0]])#np.array([2,4,5,6,7])
-br_search_locs = np.array([[0,2],[1,1],[2,0]]) #np.array([5,6,7])
-b_search_locs = np.array([[0,2],[1,1],[2,0],[1,-1],[0,-2]]) #np.array([5,6,7,8,1])#
-bl_search_locs = np.array([[2,0],[1,-1],[0,-2]]) #np.array([7,8,1])#
+l_search_locs = np.array([[2,0],[1,-1],[0,-2],[-1,-1],[-2,0]])
+tl_search_locs = np.array([[0,-2],[-1,-1],[-2,0]])
+t_search_locs = np.array([[0,-2],[-1,-1],[-2,0],[-1,1],[0,2]])
+tr_search_locs = np.array([[-2,0],[-1,1],[0,2]])
+r_search_locs = np.array([[-2,0],[-1,1],[0,2],[1,1],[2,0]])
+br_search_locs = np.array([[0,2],[1,1],[2,0]])
+b_search_locs = np.array([[0,2],[1,1],[2,0],[1,-1],[0,-2]])
+bl_search_locs = np.array([[2,0],[1,-1],[0,-2]])
 locs = [c_search_locs,l_search_locs,tl_search_locs,t_search_locs,tr_search_locs,r_search_locs,br_search_locs,b_search_locs,bl_search_locs]
+
+l_shift = np.array([[0,5],[1,0],[2,4],[8,6]])
+l_new = np.array([7,8,1,2,3])
+tl_shift = np.array([[4,5],[3,4],[0,6],[2,0],[8,7],[1,8]])
+tl_new = np.array([1,2,3])
+t_shift = np.array([[0,7],[3,0],[4,6],[2,8]])
+t_new = np.array([1,2,3,4,5])
+tr_shift = np.array([[0,8],[4,0],[2,1],[3,2],[6,7],[5,6]])
+tr_new = np.array([3,4,5])
+r_shift = np.array([[0,1],[4,2],[5,0],[6,8]])
+r_new = np.array([3,4,5,6,7])
+br_shift = np.array([[0,2],[6,0],[8,1],[4,3],[5,4],[7,8]])
+br_new = np.array([5,6,7])
+b_shift = np.array([[0,3],[8,2],[7,0],[6,4]])
+b_new = np.array([5,6,7,8,1])
+bl_shift = np.array([[0,4],[8,0],[2,3],[1,2],[6,5],[7,6]])
+bl_new = np.array([7,8,1])
+shifts = [None,l_shift,tl_shift,t_shift,tr_shift,r_shift,br_shift,b_shift,bl_shift]
+news = [None,l_new,tl_new,t_new,tr_new,r_new,br_new,b_new,bl_new]
 # start loop
 while True:
     # try to read a frame
@@ -71,6 +90,8 @@ while True:
     img = cv2.flip(img,1)
     curr_rect_px = img[start_point[1]:end_point[1],start_point[0]:end_point[0]]
     
+    eff = False
+
     # === Rectangle tracking update ===
     # update position
     first_frame = True
@@ -89,7 +110,8 @@ while True:
         # keep running the search as long as in the search space
         #print("start")
         while y_off >= -SEARCH_SIZE and y_off <= SEARCH_SIZE and x_off >= -SEARCH_SIZE and x_off <= SEARCH_SIZE:
-            #print(f"------------iteration: {iteration}")
+            print(f"------------iteration: {iteration}")
+            # print(search_locs)
             # iterate over the latest diamond points (some already computed)
             for i,coord in enumerate(search_locs):
                 count += 1
@@ -98,18 +120,23 @@ while True:
                     print("Hitting Boundary")
                 else:
                     pred_px = img[start_point[1]+y_off+coord[0]:end_point[1]+y_off+coord[0],start_point[0]+x_off+coord[1]:end_point[0]+x_off+coord[1]]
-                    l1[i] = np.sum(np.abs(pred_px.astype(int)-last_rect_px.astype(int)))
+                    if eff and next_dir > 0:
+                        l1[news[next_dir][i]] = np.sum(np.abs(pred_px.astype(int)-last_rect_px.astype(int)))
+                    else:
+                        l1[i] = np.sum(np.abs(pred_px.astype(int)-last_rect_px.astype(int)))
             last_dir = next_dir
             next_dir = np.argmin(l1)
-            # print(f"last: {last_dir}, next: {next_dir}")
-            # print(f"x_off: {x_off}, y_off: {y_off}")
+            print(l1)
+            print(f"last: {last_dir}, next: {next_dir}")
+            #rint(f"x_off: {x_off}, y_off: {y_off}")
             # need to handle how to update the l1 for next dir, also need to break if next dir is center after one more iter
             # update current offset
-            x_off += search_locs[next_dir][1]
-            y_off += search_locs[next_dir][0]
+            x_off += all_search_locs[next_dir][1]
+            y_off += all_search_locs[next_dir][0]
 
             # update next search locations
-            #search_locs = locs[next_dir]
+            if eff and next_dir != 0:
+                search_locs = locs[next_dir] # this seems to be the issue
 
             # termination case, if center is best
             if last_dir == 0:
@@ -117,9 +144,12 @@ while True:
 
             # other wise need to fill in precomputed distances
             else:
-                pass
+                # save differences we can reuse
+                if eff and next_dir != 0:
+                    for i,shift in enumerate(shifts[next_dir]):
+                        l1[shift[1]] = l1[shift[0]]
             iteration += 1
-        # print(f"best match: ({y_off},{x_off}) after {count} differences")      
+        print(f"best match: ({y_off},{x_off}) after {count} differences")      
         # exit()
         # print(count)
         # closest = np.argmin(l1)
@@ -129,10 +159,10 @@ while True:
         # x_rel = col-SEARCH_SIZE
         # print(row,col)
         # print(dirs[closest])
-        start_point[0] += x_off#dirs[closest][0]*RECT_W/factor
-        start_point[1] += y_off#dirs[closest][1]*RECT_W/factor
-        end_point[0] += x_off#dirs[closest][0]*RECT_W/factor
-        end_point[1] += y_off#dirs[closest][1]*RECT_W/factor
+        start_point[0] += x_off
+        start_point[1] += y_off
+        end_point[0] += x_off
+        end_point[1] += y_off
     else:
         min_noise = np.sum(np.abs(curr_rect_px-last_rect_px))
 